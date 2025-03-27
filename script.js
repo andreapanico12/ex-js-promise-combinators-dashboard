ac125d8fa955caf844d31e2ee6d11d1f00b05e40
andreapanico12 <andreapanico12@gmail.com>
2025-03-27 10:41:27 +0100

    Refactor getDashboardData to handle errors and improve data logging

--- script.js
+++ script.js
@@ -17,25 +17,40 @@ async function fetchJason(url){
 
 async function getDashboardData(query){
 
+let dashboard
 
+try {
   const fetchCity = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`)
 
   const fetchWeather = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`)
 
   const fetchAirport = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`)
-  const result = await Promise.all([fetchCity,fetchWeather,fetchAirport])
-  
-  let dashboard ={ 
+  const result = await Promise.all
+  ([fetchCity,fetchWeather,fetchAirport])
+
+  dashboard ={ 
     city : result[0][0].name,
     country: result[0][0].country,
     temperature: result[1][0].temperature,
     weather:result[1][0].weather_description,
     airport:result[2][0].name
-
   }
+}
+catch(error){
+  throw new Error(`Errore nel completamento della richiesta`)
+}
+return dashboard
 
-  return console.log(dashboard)
  
 }
 
   getDashboardData("london")
+  .then(data => {
+    console.log(`Dashboard data:`,data);
+    console.log(
+      `${data.city} is in ${data.country}.\n` +
+      `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+      `The main airport is ${data.airport}.\n`
+      );
+  })
+  .catch(error => console.error(error.message))
