fde56d299483196585ff63d88fb647e2d689ac08
andreapanico12 <andreapanico12@gmail.com>
2025-03-27 11:13:47 +0100

    Enhance getDashboardData to handle empty results and improve data logging

--- script.js
+++ script.js
@@ -23,17 +23,24 @@ try {
   const fetchCity = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`)
 
   const fetchWeather = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`)
+  console.log(fetchWeather)
 
   const fetchAirport = fetchJason(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`)
   const result = await Promise.all
   ([fetchCity,fetchWeather,fetchAirport])
+  
+ for(let i = 0; i <= result.length -1; i++){
+  if(result[i].length === 0){
+      result[i] = null;
+  }
+ }
 
   dashboard ={ 
-    city : result[0][0].name,
-    country: result[0][0].country,
-    temperature: result[1][0].temperature,
-    weather:result[1][0].weather_description,
-    airport:result[2][0].name
+    city : result[0]? result[0][0].name : null,
+    country: result[0]? result[0][0].country : null,
+    temperature: result[1]? result[1][0].temperature : null,
+    weather:result[1]? result[1][0].weather_description : null,
+    airport:result[2]? result[2][0].name: null
   }
 }
 catch(error){
@@ -44,13 +51,13 @@ return dashboard
  
 }
 
-  getDashboardData("london")
+  getDashboardData("vienna")
   .then(data => {
     console.log(`Dashboard data:`,data);
     console.log(
-      `${data.city} is in ${data.country}.\n` +
-      `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
-      `The main airport is ${data.airport}.\n`
+      (data.city && data.country ? `${data.city} is in ${data.country}.\n` : "") +
+      (data.temperature && data.weather ? `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` : "" ) +
+      (data.airport ? `The main airport is ${data.airport}.\n` : "" )
       );
   })
   .catch(error => console.error(error.message))
